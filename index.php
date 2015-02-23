c30e86b1ae8725b0ff61f813c23bc21b848325c5
MTardchild <Marius.Tuschl@googlemail.com>
2015-02-23 17:07:49 +0100

    Revert "Revert "Added new session variable rights""
    This reverts commit 8463d77f4a6f76fcae4f8a3cc3464cc474c5dbaa.

--- index.php
+++ index.php
@@ -42,55 +42,11 @@
 			<div id="middle">
 				<div id="welcomemessage">
 					<div class="smallcard">
-						<table class="contenttable">
-							<thead>
-								<tr>
-									<th colspan="2">Assign Task</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>
-										<form action="tasks-assign.php" class="adduser" method="post">
-											<b>Trainee</b><br>
-											Usually left default.
-											<?php													
-												// Connect to SQL
-												$connection = new mysqli($MySQLHost, $MySQLUser , $MySQLPass, $MySQLDB);
-												if ($connection->connect_error) {
-													die('Connect Error (' . $connection->connect_errno . ') '
-													. $connection->connect_error);
-												}
-
-												$query = $connection->query('SELECT user_id, firstname FROM users');
-												
-												echo '<select name=\'trainee\'><option value=\'default\'>Default</option>';
-												while ($row = $query->fetch_array(MYSQL_ASSOC)) {
-												echo '<option value=\'' . $row['user_id'] . '\'>' . $row['user_id'] . ', ' . $row['firstname'] . '</option>';
-												  }
-													echo '</select>';
-											?>	
-											<b>Task</b><br>
-											<?php													
-												$query = $connection->query('SELECT * FROM tasks');
-												
-												echo '<select name=\'task\'><option name=\'default\' value=\'default\' selected>Please Select Task</option>';
-												while ($row = $query->fetch_array(MYSQL_ASSOC)) {
-												echo '<option value=\'' . $row['user_id'] . '\'>' . $row['name'] . ', ' . $row['score']; 
-												if ($row['special'] == 1) {
-													echo ', Special';
-												}
-												echo '</option>';
-												  }
-													echo '</select>';
-												$connection->close(); 
-											?>	
-											<input type="submit" value="Assign">
-										</form>
-									</td>
-								</tr>
-							</tbody>
-						</table>
+					<?php 
+						if($_SESSION['rights'] == 1) {
+							include('task-assign-table.html');
+						}
+					?>
 					</div>
 					<div class="smallcard">
 						<div id="welcomestring">
